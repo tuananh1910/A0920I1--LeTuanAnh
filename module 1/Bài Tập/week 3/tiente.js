abbdf4b8efd3e16f045bef8097ec8bcb7f64d193
tuananh1910 <anht19102000@gmail.com>
2020-10-10 13:29:54 +0700

    tuan anh commit bai tap

--- module 1/Bài Tập/week 3/tiente.js
+++ module 1/Bài Tập/week 3/tiente.js
@@ -1,7 +1,19 @@
 function convert() {
 
-    let x= document.getElementById('1').value
-    let y= parseInt(x)
-    let dollar = y/23000
-    alert("Đổi ra Dollar=" +dollar)
+    let x= document.getElementById('1').value;
+    let y= parseInt(x);
+    let fromCurrent= parseInt(document.getElementById('fromCurrent').value);
+    let toCurrent= parseInt(document.getElementById('toCurrent').value);
+    let current;
+    if (fromCurrent === Viet Nam && toCurrent === USD){
+        current= y/23000;
+        alert(current);
+    }
+    else if(fromCurrent === USD && toCurrent === Viet Nam){
+        current= y*23000;
+        alert(current);
+    }
+    else{
+        alert('nhap loai tien muon chuyen doi')
+    }
 }
