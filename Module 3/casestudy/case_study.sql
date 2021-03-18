e73c09ecc208cc29d742d7cb439b3b9adbdc9269
tuananh1910 <anht19102000@gmail.com>
2021-03-18 21:25:23 +0700

    tuan anh commit bai tap

--- Module 3/casestudy/case_study.sql
+++ Module 3/casestudy/case_study.sql
@@ -126,18 +126,21 @@ INSERT INTO ViTri VALUES ('3', 'Manage');
 INSERT INTO DichVuDiKem VALUES ('1', 'Buffet', '10', '5', 'on');
 INSERT INTO DichVuDiKem VALUES ('2', 'Rent Car', '50', '25', 'on');
 INSERT INTO DichVuDiKem VALUES ('3', 'Party', '100', '50', 'on');
+
 -- table kieu thue
 INSERT INTO KieuThue VALUES ('1', 'Villa', '500');
 INSERT INTO KieuThue VALUES ('2', 'Home', '300');
 INSERT INTO KieuThue VALUES ('3', 'House', '200');
 -- table loai dich vu
 INSERT INTO LoaiDichVu VALUES ('1', 'Villa');
-INSERT INTO LoaiDichVu VALUES ('2', 'House');
-INSERT INTO LoaiDichVu VALUES ('3', 'Room');
+INSERT INTO LoaiDichVu VALUES ('2', 'Home');
+INSERT INTO LoaiDichVu VALUES ('3', 'House');
+INSERT INTO LoaiDichVu VALUES ('4','Spa');
 -- table loai khach
-INSERT INTO LoaiKhach (`IDLoaiKhach`, `TenLoaiKhach`) VALUES ('1', 'Diamond');
-INSERT INTO LoaiKhach (`IDLoaiKhach`, `TenLoaiKhach`) VALUES ('2', 'Vip');
-INSERT INTO LoaiKhach (`IDLoaiKhach`, `TenLoaiKhach`) VALUES ('3', 'Normal');
+INSERT INTO LoaiKhach VALUES ('1', 'Diamond');
+INSERT INTO LoaiKhach  VALUES ('2', 'Vip');
+INSERT INTO LoaiKhach  VALUES ('3', 'Normal');
+INSERT INTO LoaiKhach  VALUES ('4', 'Diamond');
 
 -- table dich vu
 INSERT INTO DichVu VALUES (1, 'Villa', '500', '5', '10', 1000, 1, 1, 'on');
@@ -149,18 +152,26 @@ INSERT INTO NhanVien VALUES ('2', 'Tran Thi T', '2', '2', '2', '2000-10-19', '12
 INSERT INTO NhanVien VALUES ('3', 'Tran Thi H', '3', '3', '3', '2000-10-19', '123132', '3000', '0123456', 'anh@gmail.com', 'Da Nang');
 -- table khach hang
 INSERT INTO KhachHang VALUES ('1', '1', 'Le Tuan Anh', '2000-10-19', '123456', 'anht@gmail.com', 'Da Nang');
-INSERT INTO KhachHang VALUES ('2', '2', 'Doan Hoang Anh', '2000-10-19', '123136', 'anh@gmail.com', 'HCM');
+INSERT INTO KhachHang VALUES ('2', '2', 'Doan Hoang Anh', '2000-10-19', '123136', 'anh@gmail.com', 'Vinh');
 INSERT INTO KhachHang VALUES ('3', '3', 'Le An Nhien', '2000-10-19', '123135', 'anh@gmail.com', 'Da Nang');
+INSERT INTO KhachHang VALUES ('4','4','Doan An Nhien','2000-10-19', '123135', 'anh@gmail.com', 'Quang Tri');
 
 -- table hop dong
-INSERT INTO HopDong VALUES ('1', '1', '1', '1', '2021-03-06', '2021-03-10', '100', '500');
-INSERT INTO HopDong VALUES ('2', '2', '2', '2', '2021-03-05', '2021-03-09', '100', '200');
-INSERT INTO HopDong VALUES ('3', '3', '3', '3', '2021-03-04', '2021-03-08', '100', '300');
-INSERT INTO HopDong VALUES ('4', '1', '1', '2', '2021-03-07', '2021-03-15', '100',300);   -- HopDong.TongTien =(DichVu.ChiPhiThue+(DichVuDiKem.DonGia*DichVuDiKem.Gia))
+INSERT INTO HopDong VALUES ('1', '1', '1', '1', '2018-03-06', '2018-03-10', '100', '500');
+INSERT INTO HopDong VALUES ('2', '2', '2', '2', '2018-03-05', '2018-02-09', '100', '200');
+INSERT INTO HopDong VALUES ('3', '3', '3', '3', '2021-01-04', '2021-01-08', '100', '300');
+INSERT INTO HopDong VALUES ('4', '1', '1', '2', '2019-01-07', '2019-10-15', '100',300);   
+INSERT INTO HopDong VALUES ('5','2','2','1','2018-10-19','2018-10-21','100','200');			-- HopDong.TongTien =(DichVu.ChiPhiThue+(DichVuDiKem.DonGia*DichVuDiKem.Gia))
+INSERT INTO HopDong VALUES ('6','2','4','2','2018-01-19','2018-01-21','100','200');
 -- table hop dong chi tiet
 INSERT INTO HopDongChiTiet VALUES ('1', '1', '1', '10');
 INSERT INTO HopDongChiTiet VALUES ('2', '2', '2', '01');
 INSERT INTO HopDongChiTiet VALUES ('3', '3', '3', '19');
+INSERT INTO HopDongChiTiet VALUES ('4', '4', '3', '5');
+INSERT INTO HopDongChiTiet VALUES ('5', '5', '3', '5');
+INSERT INTO HopDongChiTiet VALUES ('6', '6', '2', '5');
+
+
 
 
 
@@ -191,13 +202,70 @@ order by SoLanDatPhong ASC;			-- sap xep theo so lan dat phong tang dan!
 
 -- task 5
 select KhachHang.IDKhachHang, KhachHang.HoTen, LoaiKhach.TenLoaiKhach, HopDong.IDHopDong, DichVu.TenDichVu, HopDong.NgayLamHopDong, HopDong.NgayKetThucHopDong,
-		( HopDong.TongTien =(DichVu.ChiPhiThue+(DichVuDiKem.DonVi*DichVuDiKem.Gia))) as TongTien
+		 (DichVu.ChiPhiThue+(DichVuDiKem.DonVi*DichVuDiKem.Gia)) as TongTien 
 from KhachHang
-left join HopDong on HopDong.IDKhachHang = KhachHang.IDKhachHang
+left join HopDong on KhachHang.IDKhachHang = HopDong.IDKhachHang
 left join LoaiKhach on LoaiKhach.IDLoaiKhach = KhachHang.IDLoaiKhach
-left join DichVu on DichVu.IDDichVu = HopDong.DichVu;
+left join DichVu on HopDong.IDDichVu = DichVu.IDDichVu
+left join HopDongChiTiet  on HopDongChiTiet.IDHopDong = HopDong.IDHopDong
+left join DichVuDiKem on DichVuDiKem.IDDichVuDiKem = HopDongChiTiet.IDDichVuDiKem ;
+
+-- HopDong.TongTien =(DichVu.ChiPhiThue+(DichVuDiKem.DonVi*DichVuDiKem.Gia))
+
+-- task 6
+select DichVu.IDDichVu, DichVu.TenDichVu, DichVu.DienTich, DichVu.ChiPhiThue, LoaiDichVu.TenLoaiDichVu, HopDong.NgayLamHopDong
+from DichVu
+join LoaiDichVu on LoaiDichVu.IDLoaiDichVu = DichVu.IDLoaiDichVu
+left join HopDong on HopDong.IDDichVu = DichVu.IDDichVu
+group by DichVu.IDDichVu, DichVu.TenDichVu, DichVu.DienTich, DichVu.ChiPhiThue, LoaiDichVu.TenLoaiDichVu,	HopDong.NgayLamHopDong
+having
+	month(HopDong.NgayLamHopDong) <> 1 and
+      year(HopDong.NgayLamHopDong) = 2019 and
+      month(HopDong.NgayLamHopDong) <> 2 and
+      month(HopDong.NgayLamHopDong) <> 3;
+-- -------
+
+-- task 7
+select DichVu.IDDichVu, DichVu.TenDichVu, DichVu.DienTich, DichVu.SoNguoiToiDa, DichVu.ChiPhiThue, LoaiDichVu.TenLoaiDichVu, year(HopDong.NgayLamHopDong),HopDong.NgayLamHopDong
+from DichVu
+join HopDong on HopDong.IDDichVu = DichVu.IDDichVu
+left join LoaiDichVu on LoaiDichVu.IDLoaiDichVu = DichVu.IDLoaiDichVu
+where year(HopDong.NgayLamHopDong)= 2018	;
+-- group by	DichVu.IDDichVu, DichVu.TenDichVu, DichVu.DienTich, DichVu.SoNguoiToiDa, DichVu.ChiPhiThue, LoaiDichVu.TenLoaiDichVu, year(HopDong.NgayLamHopDong);																																		
+-- having  year(HopDong.NgayLamHopDong) <> 2019 and
+-- 	 year(HopDong.NgayLamHopDong);
+--       
+--       
+-- task 8
+select KhachHang.HoTen from KhachHang
+group by KhachHang.HoTen ;
+
+-- task 9
+select year(HopDong.NgayLamHopDong) as Nam  , month(HopDong.NgayLamHopDong) as Thang, count(HopDong.IDKhachHang) as SoLuongKhachHangDat
+from HopDong
+group by year(HopDong.NgayLamHopDong) , month(HopDong.NgayLamHopDong);
+
+-- task 10
+select HopDong.IDHopDong, HopDong.NgayLamHopDong, HopDong.NgayKetThucHopDong, HopDong.TienDatCoc, count(HopDongChiTiet.IDHopDongChiTiet) as SoLuongDVDK
+from HopDong
+left join HopDongChiTiet on HopDongChiTiet.IDHopDong = HopDong.IDHopDong
+group by HopDong.IDHopDong, HopDong.NgayLamHopDong, HopDong.NgayKetThucHopDong, HopDong.TienDatCoc;
+
+-- task 11
+select * from DichVuDiKem
+left join HopDongChiTiet on HopDongChiTiet.IDDichVuDiKem = DichVuDiKem.IDDichVuDiKem
+left join HopDong on HopDong.IDHopDong = HopDongChiTiet.IDHopDong
+left join KhachHang on KhachHang.IDKhachHang = HopDong.IDKhachHang
+left join LoaiKhach on LoaiKhach.IDLoaiKhach = KhachHang.IDLoaiKhach
+where LoaiKhach.TenLoaiKhach ='Diamond'
+group by HopDong.IDKhachHang
+having KhachHang.DiaChi = 'Quang Tri' or
+	KhachHang.DiaChi = 'Vinh';
+      
+-- task 12
 
 
+  
 
 
 
