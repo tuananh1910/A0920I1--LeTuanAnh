60a1a4a34ebecdd0377b62569665903a5ecaf6e3
someone <you@example.com>
2021-07-23 21:16:41 +0700

    add module 5

---
+++ Module 5/Bai 3/angular-component/src/app/angular-overview/angular-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-overview',
+  templateUrl: './angular-overview.component.html',
+  styleUrls: ['./angular-overview.component.css']
+})
+export class AngularOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
